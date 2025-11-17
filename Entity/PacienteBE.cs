01f15b0b62bf07d9ba26a4068d7349cd9385c0aa
AlanGB2001 <alabbustamante@gmail.com>
2025-11-16 21:14:21 -0300

    Cree Todas las BLL
    Cree las BLL de turno, Paciente y Nutricionista

---
+++ Entity/PacienteBE.cs
@@ -0,0 +1,14 @@
+﻿namespace Entity
+{
+    public class PacienteBE
+    {
+        public int IdPaciente { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime FechaNacimiento {  get; set; }
+        public string DNI { get; set; }
+        public string Mail { get; set; }
+        public string Telefono { get; set; }
+
+    }
+}
---
+++ Entity/TurnoBE.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class TurnoBE
+    {
+        public int IDTurno {get;set;}
+        public DateTime FechaHora{get;set;}
+        public string Detalle {get;set;}
+        public string Estado {get;set;}
+        public PacienteBE Paciente {get;set;}
+        public NutricionistaBE Nutricionista {get;set;}
+    }
+}
